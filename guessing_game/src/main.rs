6ecfed337bdb4cd73d6bdb8544933cbea6650dfc
Michael Toth <mtoth@way2b1.com>
2018-01-24 15:56:24 -0800

    Wait that wasn't right

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,38 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Guess the number");
+
+    loop {
+        println!("Please enter your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) =>  {
+                println!("Not a number bruh");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Just right!");
+                break;
+            },
+        }
+    }
 }
