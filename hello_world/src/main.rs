d0083a39216d4fdee40fb9c399bed91e2539f36a
Michael Toth <mtoth@way2b1.com>
2018-01-25 16:09:03 -0800

    i understand now
    had cached targets already included

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("hello world");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+    println!("Hello, World.");
+    println!("{}", input);
 }
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/michael-toth/workspace/rust/30_days_of_rust/hello_world/target/debug/hello_world.dSYM: /Users/michael-toth/workspace/rust/30_days_of_rust/hello_world/src/main.rs
